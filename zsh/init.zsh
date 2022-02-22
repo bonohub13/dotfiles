4b2f4b8522d807b1f6e12aa1b0af052944ad6d0f
Kensuke Saito <ken.saito.0813@gmail.com>
2022-02-22 19:24:50 +0900

    Commented out line for ssh agent initailization

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,7 +1,10 @@
 #!/usr/bin/zsh
 
 ### init runs #####
-. ~/.config/zsh/.commands/run_sshAgent.sh
+#: Autoinitializes ssh agent to keep the passwords
+#: for clients/hosts that have the public key of this
+#: device. (commented out by default)
+# . ~/.config/zsh/.commands/run_sshAgent.sh
 source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 command -v neofetch > /dev/null 2>&1 \
     && command -v lolcat > /dev/null 2>&1 \
